61f2607d848178c30a32e258c4f2f8b723735777
bclar <bc013@email.uark.edu>
2019-04-13 16:31:24 -0500

    commit

--- Provider.sql
+++ Provider.sql
@@ -6,7 +6,6 @@ CREATE TABLE provider (
   state character varying(15) NOT NULL DEFAULT(''),
   zip int NOT NULL,
   provider_id int NOT NULL DEFAULT(1),
-  role character varying(32) NOT NULL CHECK (role in ('customer', 'service provider')) DEFAULT('service provider'),
   phone character varying(15) NOT NULL DEFAULT(''),
   email character varying(50) NOT NULL DEFAULT(''),
   password character varying(20) NOT NULL DEFAULT(''),
