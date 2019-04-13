817a37585d481daa6b78bc20df2c54becabd7dcb
nathanmangrum <46819021+nathanmangrum@users.noreply.github.com>
2019-04-13 11:55:57 -0500

    Update services.sql

--- services.sql
+++ services.sql
@@ -1,6 +1,6 @@
 CREATE TABLE services(
   service_name character varying(25) NOT NULL DEFAULT(''),
-  service_id int NOT NULL DEFAULT(1),
   provider_id int NOT NULL,
-  CONSTRAINT service_pkey PRIMARY KEY(provider_id)
+  CONSTRAINT service_pkey PRIMARY KEY(service_name),
+  FOREIGN KEY (provider_id) REFERENCES provider(provider_id)
 );
