4405720716f9b6e6541608e3178de35a4a413315
nathanmangrum <46819021+nathanmangrum@users.noreply.github.com>
2019-04-13 10:24:53 -0500

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -1,9 +1,15 @@
 CREATE TABLE customer (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
-  first character varying(15) NOT NULL DEFAULT(''),
-  last character varying(15) NOT NULL DEFAULT(''),
+  first_name character varying(15) NOT NULL DEFAULT(''),
+  last_name character varying(15) NOT NULL DEFAULT(''),
   address character varying(50) NOT NULL DEFAULT(''),
   city character varying(25) NOT NULL DEFAULT(''),
   state character varying(15) NOT NULL DEFAULT(''),
-  zip int NOT NULL DEFAULT('')
+  zip int NOT NULL DEFAULT(''),
+  password character varying(255) NOT NULL,
+  customer_id int NOT NULL DEFAULT(1),
+  role character varying(32) NOT NULL CHECK (role in ('customer', 'service provider')) DEFAULT ('customer')
+  CONSTRAINT customer_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
 );
