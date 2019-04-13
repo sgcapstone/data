1609d6c22d20d9193bb1d55775db214f58e2eda6
nathanmangrum <46819021+nathanmangrum@users.noreply.github.com>
2019-04-13 11:37:36 -0500

    Add files via upload

---
+++ provider.sql
@@ -0,0 +1,16 @@
+CREATE TABLE provider (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  provider_name character varying(50) NOT NULL DEFAULT(''),
+  address character varying(50) NOT NULL DEFAULT(''),
+  city character varying(20) NOT NULL DEFAULT(''),
+  state character varying(15) NOT NULL DEFAULT(''),
+  zip int NOT NULL,
+  provider_id int NOT NULL DEFAULT(1),
+  role character varying(32) NOT NULL CHECK (role in ('customer', 'service provider')) DEFAULT('service provider'),
+  phone character varying(15) NOT NULL DEFAULT(''),
+  email character varying(50) NOT NULL DEFAULT(''),
+  CONSTRAINT provider_pkey PRIMARY KEY(id),
+  FOREIGN KEY (provider_id) REFERENCES services (provider_id)
+) WITH (
+  OIDS=FALSE
+);
