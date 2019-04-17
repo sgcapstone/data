71e24c74d0f69f0f81614574db022e5f0ee1a8cb
bclar <bc013@email.uark.edu>
2019-04-16 20:06:22 -0500

    commit

--- fix.sql
+++ fix.sql
@@ -26,13 +26,13 @@ CREATE TABLE providers (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   provider_name character varying(50) NOT NULL DEFAULT('Provider'),
   address character varying(50) NOT NULL DEFAULT('1111 B St'),
-  city character varying(20) NOT NULL DEFAULT('City'),
+  city character varying(25) NOT NULL DEFAULT('City'),
   state character varying(15) NOT NULL DEFAULT('State'),
   zip int NOT NULL DEFAULT(12345),
   provider_id int NOT NULL UNIQUE,
   phone character varying(15) NOT NULL DEFAULT('18008675309'),
   email character varying(50) NOT NULL DEFAULT('provider@provider.com'),
-  password character varying(20) NOT NULL DEFAULT('password'),
+  password character varying(255) NOT NULL DEFAULT('password'),
   created_at timestamp without time zone NOT NULL DEFAULT now(),
   updated_at timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT provider_pkey PRIMARY KEY(id)
@@ -57,6 +57,9 @@ CREATE TABLE services (
 INSERT INTO consumers (first_name, last_name, address, city, state, zip, password, customer_id) VALUES ('First', 'Last', '1234 A St', 'City', 'State', '12345', 'password', 1) RETURNING id, created_at;
 
 INSERT INTO providers (provider_id) VALUES (1) RETURNING id, created_at;
+INSERT INTO providers (provider_id) VALUES (2) RETURNING id, created_at;
+INSERT INTO providers (provider_id) VALUES (3) RETURNING id, created_at;
 
 INSERT INTO services (service_name, provider_id) VALUES ('mowing', 1) RETURNING id, created_at;
 INSERT INTO services (service_name, provider_id) VALUES ('weed-eating', 1) RETURNING id, created_at;
+INSERT INTO services (service_name, provider_id) VALUES ('mowing', 3) RETURNING id, created_at;
